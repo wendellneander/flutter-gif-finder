43e6636b523013778e6c5cab411a56e6ff730a8f
Wendell Neander <neanderdev@gmail.com>
2018-09-16 17:40:34 -0300

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:gif_finder/ui/hom_page.dart';
+
+void main() => runApp(MaterialApp(
+  home: HomePage(),
+  theme: ThemeData(hintColor: Colors.white),
+));
